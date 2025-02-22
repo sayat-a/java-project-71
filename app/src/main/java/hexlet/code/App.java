eda5711b7bfa0f1a5dc9b4a1c833d67f5e6f46dc
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2025-02-23 00:01:43 +0500

    update src/main/java/hexlet/code/App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+    @Override
+    public void run() {
+        // Если никакие аргументы не переданы, просто вывести сообщение
+        System.out.println("Please provide two files to compare.");
     }
 }
